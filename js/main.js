b86110061f53940face4e85806ee510ac6a20517
MacKenzie Bulkowski-Rose <mackenzie.bulkowski.rose@gmail.com>
2020-03-10 11:23:31 -0400

    Finish array content

--- js/main.js
+++ js/main.js
@@ -32,6 +32,17 @@ var insects = [
       eatsPlants: true
     },
   },
+  {
+    name: 'Tortoise Beetle',
+    img: 'tortoise-beetle.jpg',
+    family: 'Cassidinae',
+    size: [1, 18],
+    food: {
+      eatsInsects: false,
+      eatsDung: false,
+      eatsPlants: true
+    },
+  }
 ];
 
 // ##########################################################
